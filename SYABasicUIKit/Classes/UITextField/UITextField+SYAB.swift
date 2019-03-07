1e42ae40d9fb0a0ac036b46e8f5992796a8769c6
xygj <wangyalong24@126.com>
2019-03-07 14:07:20 +0800

    修改错误

--- SYABasicUIKit/Classes/UITextField/UITextField+SYAB.swift
+++ SYABasicUIKit/Classes/UITextField/UITextField+SYAB.swift
@@ -9,11 +9,11 @@ import Foundation
 
 public extension UITextField {
     
-    public func syab_placeholder(text: String, textColor: UIColor, textFont: UIFont) -> Void {
-        let attributedText = NSMutableAttributedString(string: text)
-        attributedText.addAttribute(NSAttributedString.Key.font, value: textFont, range: NSRange(location: 0, length: text.count))
-        attributedText.addAttribute(NSAttributedString.Key.foregroundColor, value: textColor, range: NSRange(location: 0, length: text.count))
-        self.attributedPlaceholder = attributedText
-    }
+//    public func syab_placeholder(text: String, textColor: UIColor, textFont: UIFont) -> Void {
+//        let attributedText = NSMutableAttributedString(string: text)
+//        attributedText.addAttribute(NSAttributedString.Key.font, value: textFont, range: NSRange(location: 0, length: text.count))
+//        attributedText.addAttribute(NSAttributedString.Key.foregroundColor, value: textColor, range: NSRange(location: 0, length: text.count))
+//        self.attributedPlaceholder = attributedText
+//    }
     
 }
